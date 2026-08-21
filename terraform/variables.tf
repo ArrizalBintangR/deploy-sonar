af358ef712916598fbdac4fba414032bbc36d274
ArrizalBintangR <arrizalbramadhan09@gmail.com>
2026-08-21 19:27:30 +0700

    add gke overlays and fix variables again

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,13 +6,13 @@ variable "project_id" {
 variable "region" {
   description = "Region for the Cloud Storage Bucket"
   type = string
-  default = "us-west1"
+  default = "europe-west1"
 }
 
 variable "zone" {
   description = "Zone for the GKE cluster. Zonal (not regional). regional cluster = replicates to 3 node pool by default."
   type = string
-  default = "us-west1-a"
+  default = "europe-west1-b"
 }
 
 variable "bucket_name" {
